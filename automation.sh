3ba072ebf8e04c3f8fd40fa97ce824b39f532325
Ubuntu <ubuntu@ip-172-31-80-153.ec2.internal>
2022-01-24 08:48:10 +0000

    intital commit

--- automation.sh
+++ automation.sh
@@ -3,6 +3,7 @@ sudo apt install apache2 -y
 systemctl status apache2
 sudo apt install awscli -y
 myname=ratnesh
+echo "Hello"
 timestamp=$(date '+%d%m%Y-%H%M%S')
 tar -czvf /tmp/${myname}-httpd-logs-${timestamp}.tar /var/log/apache2/access.log
 tar -czvf /tmp/${myname}-httpd-logs1-${timestamp}.tar /var/log/apache2/error.log
